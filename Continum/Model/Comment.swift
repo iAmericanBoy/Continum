b287703b1cf8ef5e73a957fd00e4811422e59c08
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-26 14:58:07 -0700

    Implments Comment model

---
+++ Continum/Model/Comment.swift
@@ -0,0 +1,21 @@
+//
+//  Comment.swift
+//  Continum
+//
+//  Created by Dominic Lanzillotta on 2/26/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+
+class Comment {
+    let text: String
+    let timeStamp: Date
+    weak var post: Post?
+    
+    init(text: String, post: Post, timeStamp: Date = Date()) {
+        self.timeStamp = timeStamp
+        self.text = text
+        self.post = post
+    }
+}
