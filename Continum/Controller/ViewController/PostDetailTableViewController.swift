62e13aa643b69680333c5fe9756bb66b7141829e
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-26 16:15:34 -0700

    Implements DataSource methods and view actions

--- Continum/Controller/ViewController/PostDetailTableViewController.swift
+++ Continum/Controller/ViewController/PostDetailTableViewController.swift
@@ -7,87 +7,87 @@
 //
 
 import UIKit
+import NotificationCenter
 
 class PostDetailTableViewController: UITableViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
     //MARK: - Properties
-    var post: Post?
+    var post: Post? {
+        didSet {
+            loadViewIfNeeded()
+            updateViews()
+        }
+    }
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    //MARK: - Actions
+    @IBAction func commentButtonTapped(_ sender: UIButton) {
+        getNewCommentAlert()
+    }
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func followPosttapped(_ sender: UIButton) {
+    }
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return post?.comments.count ?? 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell", for: indexPath)
+        
+        cell.textLabel?.text = post?.comments[indexPath.row].text
+        cell.detailTextLabel?.text = post?.comments[indexPath.row].timeStamp.description(with: Locale.current)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    //MARK: - Private Functions
+    func updateViews() {
+        guard let post = post else {return}
+        self.postImageView.image = post.photo
+        self.captionLabel.text = post.caption
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func getNewCommentAlert() {
+        var commentTextField: UITextField?
+        
+        let commentAlert = UIAlertController(title: "New Comment", message: "Enter a new Comment", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let confirmAction = UIAlertAction(title: "Post Coment", style: .default) { [weak self] action in
+            guard let post = self?.post else {return}
+            if let comment = commentTextField?.text {
+                PostController.shared.addCommentTo(post: post, withText: comment, completion: { comment in
+                    
+                })
+            }
+        }
+        commentAlert.addTextField { textField in
+            textField.placeholder = "Add Comment..."
+            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: .main) { notif in
+                if let name = textField.text, !name.isEmpty {
+                    confirmAction.isEnabled = true
+                    commentTextField = textField
+                } else {
+                    confirmAction.isEnabled = false
+                }
+            }
+        }
+        
+        commentAlert.addAction(cancelAction)
+        commentAlert.addAction(confirmAction)
+        self.present(commentAlert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
